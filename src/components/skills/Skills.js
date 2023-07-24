2e2a8a3800cfa776d328c9a01e14be9fe87e29d1
jrob02180 <jrob02180@gmail.com>
2023-07-23 21:46:29 -0600

    Added bootcamp credential and animation and adjusted size of skill icons

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,5 +1,7 @@
 import React from "react";
 // import resume from "../assets/images/Resume.pdf";
+import badge from "../../assets/badge/badge.png";
+import 'animate.css'
 
 export default function Resume() {
   return (
@@ -7,7 +9,12 @@ export default function Resume() {
       <h1 className="section-title" id="skills">
         Skills
       </h1>
-      <div className="row resume text-center">
+      <div className="credentials">
+        <div className="badge animate__rotateIn">
+          <img src={badge} alt="badge" />
+        </div>
+      </div>
+      <div className="skillIcons">
         {/* <a href={ resume } style={{ fontSize: 50 }} download>Download my resume</a> */}
         <p align="center">
           {/* <a href=""> */}
