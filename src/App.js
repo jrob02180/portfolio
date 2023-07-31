7ca5134e121d0897f7ea539dce5d69ef83dc102b
jrob02180 <jrob02180@gmail.com>
2023-07-30 19:03:49 -0600

    Finished contact section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import WOW from 'wowjs';
 import NavBar from './components/navbar/NavBar';
-// import Header from './components/header/Header';
 import About from './components/about/About';
 import Projects from './components/projects/Projects';
 import Skills from './components/skills/Skills';
 import Contact from './components/contact/Contact';
-import Footer from './components/footer/Footer';
 // import './App.css';
 import './index.css'
 
@@ -18,7 +15,6 @@ function App() {
       <Projects/>
       <Skills/>
       <Contact/>
-      <Footer/>
     </div>
   );
 }
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,85 +1,30 @@
-import React, { useState } from 'react';
+import React from "react";
+import github from "../../assets/contact/github.png";
+import linkedin from "../../assets/contact/linkedin.png";
+import insta from "../../assets/contact/instagram.png";
+import email from "../../assets/contact/email.png";
 
-import { validateEmail } from '../../utils/helpers';
-
-function Contact() {
-  const [formState, setFormState] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
-
-  const [errorMessage, setErrorMessage] = useState('');
-  const { name, email, message } = formState;
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!errorMessage) {
-      console.log('Submit Form', formState);
-    }
-  };
-
-  const handleChange = (e) => {
-    if (e.target.name === 'email') {
-      const isValid = validateEmail(e.target.value);
-      if (!isValid) {
-        setErrorMessage('Your email is invalid.');
-      } else {
-        setErrorMessage('');
-      }
-    } else {
-      if (!e.target.value.length) {
-        setErrorMessage(`${e.target.name} is required.`);
-      } else {
-        setErrorMessage('');
-      }
-    }
-    if (!errorMessage) {
-      setFormState({ ...formState, [e.target.name]: e.target.value });
-      console.log('Handle Form', formState);
-    }
-  };
-
+export default function Contact() {
   return (
-    <section>
-      <form id="contact" onSubmit={handleSubmit}>
-        <h1 className="section-title ff-secondary">Contact Me</h1>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            name="name"
-            defaultValue={name}
-            onBlur={handleChange}
-          />
+    <div>
+      <h1 id="contact" className="section-title">
+        Contact me
+      </h1>
+        <div className="social">
+          <a href="https://github.com/jrob02180">
+            <img src={github} alt="github" />
+          </a>
+
+          <a href="https://www.linkedin.com/in/jen-roberson-060417276/">
+            <img src={linkedin} alt="linkedin" />
+          </a>
+          <a href="https://www.instagram.com/jrob02180">
+            <img src={insta} alt="instagram" />
+          </a>
+          <a href="mailto:jrob02180@gmail.com">
+            <img src={email} alt="email" />
+          </a>
         </div>
-        <div>
-          <label htmlFor="email">Email address:</label>
-          <input
-            type="email"
-            name="email"
-            defaultValue={email}
-            onBlur={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea
-            name="message"
-            rows="5"
-            defaultValue={message}
-            onBlur={handleChange}
-          />
-        </div>
-        {errorMessage && (
-          <div>
-            <p className="error-text">{errorMessage}</p>
-          </div>
-        )}
-        <button type="submit">Submit</button>
-      </form>
-    </section>
+    </div>
   );
 }
-
-export default Contact;
